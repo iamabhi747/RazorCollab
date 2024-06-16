2ae608d49d418f40820294adfdd6ffea8539f869
Javaboiiii <devrajlokhande1610@gmail.com>
2024-06-16 13:39:37 +0530

    package installation and addition of the code editor

---
+++ src/components/Editor/Editor.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { UnControlled as CodeMirror } from 'react-codemirror2'
+import 'codemirror/mode/markdown/markdown'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/dracula.css'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/closetag'
+
+const Editor = () => {
+    const [value, setValue] = useState()
+  return (
+    <div>
+      <CodeMirror
+        value = {value}
+        options={{
+          mode: 'markdown',
+          theme: 'dracula',
+          autoCloseBrackets: true, 
+          autoCloseTags: true,
+          lineNumbers: true
+        }}
+        cursor={{
+            line: 1,
+            ch: 0
+          }}
+        onChange={(editor, data, value) => {
+
+        }}
+      />
+    </div>
+  )
+}
+
+export default Editor
