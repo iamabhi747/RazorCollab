d6b7688fab429135cd0f160421c30d3cbda2ec04
Javaboiiii <devrajlokhande1610@gmail.com>
2024-06-24 19:09:30 +0530

    Normal package setup and code setup some error needed to fix

---
+++ server/app.js
@@ -0,0 +1,67 @@
+import express, { static as static_ } from 'express';
+const app = express();
+import { createServer } from 'http';
+import { join } from 'path';
+import { Server } from 'socket.io';
+import { JOIN, JOINED, CODE_CHANGE, SYNC_CODE, DISCONNECTED } from '../src/Actions.js';
+
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(static_('build'));
+app.use((req, res, next) => {
+    res.sendFile(join(__dirname, 'build', 'index.html'));
+});
+
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+    // Map
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+        (socketId) => {
+            return {
+                socketId,
+                username: userSocketMap[socketId],
+            };
+        }
+    );
+}
+
+io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+
+    socket.on(JOIN, ({ roomId, username }) => {
+        userSocketMap[socket.id] = username;
+        socket.join(roomId);
+        const clients = getAllConnectedClients(roomId);
+        clients.forEach(({ socketId }) => {
+            io.to(socketId).emit(JOINED, {
+                clients,
+                username,
+                socketId: socket.id,
+            });
+        });
+    });
+
+    socket.on(CODE_CHANGE, ({ roomId, code }) => {
+        socket.in(roomId).emit(CODE_CHANGE, { code });
+    });
+
+    socket.on(SYNC_CODE, ({ socketId, code }) => {
+        io.to(socketId).emit(CODE_CHANGE, { code });
+    });
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(DISCONNECTED, {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+            });
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,30 +1,57 @@
-import React, { useState } from 'react'
-import { UnControlled as CodeMirror } from 'react-codemirror2'
-import 'codemirror/mode/markdown/markdown'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/dracula.css'
-import 'codemirror/addon/edit/closebrackets'
-import 'codemirror/addon/edit/closetag'
+import React, { useEffect, useRef } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import ACTIONS from '../Actions';
 
-const Editor = () => {
-    const [value, setValue] = useState()
-  return (
-    <div>
-      <CodeMirror className='text-2xl'
-        value = {value}
-        options={{
-          mode: 'markdown',
-          theme: 'dracula',
-          autoCloseBrackets: true, 
-          autoCloseTags: true,
-          lineNumbers: true
-        }}
-        onChange={(editor, data, value) => {
+const Editor = ({ socketRef, roomId, onCodeChange }) => {
+    const editorRef = useRef(null);
+    useEffect(() => {
+        async function init() {
+            editorRef.current = Codemirror.fromTextArea(
+                document.getElementById('realtimeEditor'),
+                {
+                    mode: { name: 'javascript', json: true },
+                    theme: 'dracula',
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    lineNumbers: true,
+                }
+            );
 
-        }}
-      />
-    </div>
-  )
-}
+            editorRef.current.on('change', (instance, changes) => {
+                const { origin } = changes;
+                const code = instance.getValue();
+                onCodeChange(code);
+                if (origin !== 'setValue') {
+                    socketRef.current.emit(ACTIONS.CODE_CHANGE, {
+                        roomId,
+                        code,
+                    });
+                }
+            });
+        }
+        init();
+    }, []);
 
-export default Editor
+    useEffect(() => {
+        if (socketRef.current) {
+            socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+                if (code !== null) {
+                    editorRef.current.setValue(code);
+                }
+            });
+        }
+
+        return () => {
+            socketRef.current.off(ACTIONS.CODE_CHANGE);
+        };
+    }, [socketRef.current]);
+
+    return <textarea id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
