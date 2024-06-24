c87b36c762c81620431220d0fcd23031fbaf0fcd
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-06-24 12:47:08 +0530

    changed output file name to code2markdown.bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "code2markdown.js",
+    filename: "code2markdown.bundle.js",
     path: path.resolve(__dirname, "build"),
     library: {
       name: "code2markdown",
