8de380d69eab8d36b57ab4b39ad3c2dc9dd310c3
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-06-27 13:39:41 +0530

    fixed boldItalics rule

--- src/rules/textStyles_bold_italics.js
+++ src/rules/textStyles_bold_italics.js
@@ -1,23 +1,24 @@
 module.exports = exports = {};
 
 exports.type="token";
+exports.order = 110;
 
 exports.rule=/(\*{1,3})(.+?)\1/;
 
 exports.resolver=function boldItalic(context,groups)
 {
-    let countAsterisk=groups[1].length;
+    let countAsterisk=groups[0].length;
     if(countAsterisk==2)
         {
-            return `<strong>${groups[2]}</strong>`;
+            return `<strong>${groups[1]}</strong>`;
         }
     else if(countAsterisk==1)
         {
-            return `<em>${groups[2]}</em>`;   
+            return `<em>${groups[1]}</em>`;   
         }
     else if(countAsterisk==3)
         {
-            return `<em><strong>${groups[2]}</strong></em>`;  
+            return `<em><strong>${groups[1]}</strong></em>`;  
         }
 
 }
