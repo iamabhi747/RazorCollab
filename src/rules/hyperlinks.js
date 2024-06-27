de9fcfe542e1e6b68567a4fa261631161d591656
anushkad-8 <anushkard884@gmail.com>
2024-06-27 14:14:23 +0530

    Added hyperlink feature

---
+++ src/rules/hyperlinks.js
@@ -0,0 +1,12 @@
+module.exports = exports = {};
+module.order=100;
+exports.type = "link";
+
+exports.rule = /\[([^\]]+)\]\(([^)]+)\)/;
+
+exports.resolver = function createLink(context, groups) {
+    let linkText = groups[0];
+    let url = groups[1];
+
+    return `<a href="${url}">${linkText}</a>`;
+};
--- src/rules/textStyles_bold_italics.js
+++ src/rules/textStyles_bold_italics.js
@@ -1,23 +1,17 @@
 module.exports = exports = {};
 
-exports.type="token";
+exports.type = "token";
 
-exports.rule=/(\*{1,3})(.+?)\1/;
+exports.rule = /(\*{1,3})(.+?)\1/;
 
-exports.resolver=function boldItalic(context,groups)
-{
-    let countAsterisk=groups[1].length;
-    if(countAsterisk==2)
-        {
-            return `<strong>${groups[2]}</strong>`;
-        }
-    else if(countAsterisk==1)
-        {
-            return `<em>${groups[2]}</em>`;   
-        }
-    else if(countAsterisk==3)
-        {
-            return `<em><strong>${groups[2]}</strong></em>`;  
-        }
+exports.resolver = function boldItalic(context, groups) {
+    let countAsterisk = groups[1].length;
 
-}
+    if (countAsterisk === 1) {
+        return `<em>${groups[2]}</em>`; // Using <em> for italic emphasis
+    } else if (countAsterisk === 2) {
+        return `<strong>${groups[2]}</strong>`; // Using <strong> for bold emphasis
+    } else if (countAsterisk === 3) {
+        return `<em><strong>${groups[2]}</strong></em>`; // Nested <em> and <strong> for bold italic emphasis
+    }
+};
