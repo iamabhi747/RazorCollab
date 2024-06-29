769ed0790cce2a8578749e2fe3168dcb52a955a7
Atharva Kavade <atharvadk27@gmail.com>
2024-06-29 11:30:25 +0530

    some minor changes

--- src/new-note/new-note.jsx
+++ src/new-note/new-note.jsx
@@ -3,28 +3,76 @@ import React from 'react';
 function NewNote() {
     const styles = {
         pageWrapper: {
+            height: '100vh',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             backgroundColor: '#333',
-        }
+        },
+        formWrapper: {
+            width: '400px',
+            maxWidth: '90%',
+            padding: '20px',
+            backgroundColor: '#444',
+            borderRadius: '10px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        },
+
+        mainLabel: {
+            marginBottom: '20px',
+        },
+        idGroup: {
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '10px',
+        },
+        titleGroup: {
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '10px',
+        },
+        inputGroup: {
+            padding: '7px',
+            borderRadius: '5px',
+            outline: 'none',
+            border: 'none',
+            backgroundColor: '#eee',
+            fontWeight: 'bold',
+
+        },
+        buttonGroup: {
+            padding: '10px',
+            borderRadius: '5px',
+            outline: 'none',
+            border: 'none',
+            backgroundColor: '#4aed88',
+            fontWeight: 'bold',
+            cursor: 'pointer',
+            marginLeft: '10px',
+        },
+
     };
 
     return (
-        <div className='PageWraper' style={styles.pageWrapper}>
-            <div className='FormWraper'>
+        <div style={styles.pageWrapper}>
+            <div style={styles.formWrapper}>
                 <form>
-                    <h1>Create a new note</h1>
-                    <div className='form-group'>
-                        <label htmlFor='title'>Title</label>
-                        <input type='text' id='title' name='title' />
+                    <div>
+                        <h1>Create a new note</h1>
                     </div>
-                    <div className='form-group'>
-                        <label htmlFor='content'>Content</label>
-                        <textarea id='content' name='content'></textarea>
+
+                    <div style={styles.idGroup}>
+                        <input type='text' id='id' name='id' placeholder='ID' style={styles.inputGroup} />
                     </div>
-                    <button type='submit'>Create</button>
+                    <div style={styles.titleGroup}>
+                        <input type='text' id='title' name='title' placeholder='Title' style={styles.inputGroup} />
+                    </div>
+
+
+                    <button type='submit' style={styles.buttonGroup}>Create</button>
+                    <button type='submit' style={styles.buttonGroup}> New ID</button>
                 </form>
+
 
             </div>
         </div>
