e3ed98ce8c9b8f26516a6521e1d5bff912df39c5
Atharva Kavade <atharvadk27@gmail.com>
2024-06-29 10:26:03 +0530

    bug fixes

--- src/new-note/new-note.jsx
+++ src/new-note/new-note.jsx
@@ -10,26 +10,25 @@ function NewNote() {
         }
     };
 
-};
-return (
-    <div className='PageWraper' style={styles.pageWrapper}>
-        <div className='FormWraper'>
-            <form>
-                <h1>Create a new note</h1>
-                <div className='form-group'>
-                    <label htmlFor='title'>Title</label>
-                    <input type='text' id='title' name='title' />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='content'>Content</label>
-                    <textarea id='content' name='content'></textarea>
-                </div>
-                <button type='submit'>Create</button>
-            </form>
+    return (
+        <div className='PageWraper' style={styles.pageWrapper}>
+            <div className='FormWraper'>
+                <form>
+                    <h1>Create a new note</h1>
+                    <div className='form-group'>
+                        <label htmlFor='title'>Title</label>
+                        <input type='text' id='title' name='title' />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='content'>Content</label>
+                        <textarea id='content' name='content'></textarea>
+                    </div>
+                    <button type='submit'>Create</button>
+                </form>
 
+            </div>
         </div>
-    </div>
-);
+    );
 
 
 
